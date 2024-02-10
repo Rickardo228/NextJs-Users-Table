bda49135dcb35ce61c9a3f3626d1939e87f2de73
RicBarnes22 <richard@protolyst.cin>
2024-02-10 19:21:43 +0000

    Moved dynamic fetch into api

--- src/api/users.ts
+++ src/api/users.ts
@@ -2,9 +2,16 @@ import { Users, User } from "./types";
 
 export const getUsers = async (): Promise<Users> => {
   "use server";
-  const users: Users = [];
-  // Todo - Make API call
-  return users;
+  const response = await fetch("https://zglinski.com/trex/users.json");
+  const users: Users = await response.json();
+
+  // Assign a unique id to each user as original data doesn't include one
+  const usersWithId = users.map((user, index) => ({
+    ...user,
+    id: index, // Todo - consider a more robust approach
+  }));
+
+  return usersWithId;
 };
 
 export const addUser = async (user: User): Promise<void> => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
 import { Users } from "@/api/types";
 import UsersTable from "./components/UsersTable";
+import { getUsers } from "@/api/users";
 
 export default async function Page() {
-  const response = await fetch("https://zglinski.com/trex/users.json");
-  const users: Users = await response.json();
-
-  // Assign a unique id to each user as original data doesn't include one
-  const usersWithId = users.map((user, index) => ({
-    ...user,
-    id: index, // Todo - consider a more robust approach
-  }));
+  const users: Users = await getUsers();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-[90%] mx-auto">
         <h1 className="text-center text-3x1 pt-5">Users Table</h1>
         <h1 className="text-center text-3x1 pt-5">by Richard Barnes</h1>
-        <UsersTable users={usersWithId} />
+        <UsersTable users={users} />
       </div>
     </main>
   );
